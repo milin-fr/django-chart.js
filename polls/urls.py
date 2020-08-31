75d9adc0a571790134834f4ba7eabd200f936f4b
unknown <m.ilin.fr@gmail.com>
2020-08-31 16:46:14 +0200

    adding json response

--- polls/urls.py
+++ polls/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 app_name = 'polls'
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+    path('<int:question_id>/get-votes/', views.get_votes, name='get_votes')
 ]
