751c13ca4eee9fe422f06b4786ab29ba793378f2
unknown <m.ilin.fr@gmail.com>
2020-08-31 09:41:49 +0200

    configuring mysql

--- sandbox/settings.py
+++ sandbox/settings.py
@@ -75,8 +75,10 @@ WSGI_APPLICATION = 'sandbox.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': './sandbox/db.cnf',
+        },
     }
 }
 
